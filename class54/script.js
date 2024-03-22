88b29b5388f9bfb5d535bcddf02ed5d2154fe69d
Abhi <abhi@abhis-MacBook-Air.local>
2024-03-22 21:17:59 +0530

    start javascript

---
+++ class54/script.js
@@ -0,0 +1,25 @@
+alert("hello wold");
+
+console.log("from console..........");
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer is not blasting")
+
+}
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+
+// document.body.style.backgroundColor = "red"
