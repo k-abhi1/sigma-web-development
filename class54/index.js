f29e8cb59a13288832f11039d506fed4d9484d24
Abhi <abhi@abhis-MacBook-Air.local>
2024-03-29 19:46:59 +0530

    java script

---
+++ class54/index.js
@@ -0,0 +1,45 @@
+// let a= prompt("enterv the first number");
+// let b= prompt("enterv the first number");
+// let r= prompt("enterv the first number");
+
+// let ch= prompt("enterv the choice");
+
+// switch (ch) {
+
+//     case 1:
+//         r = a + b;
+//         console.log(r);
+//         break;
+
+//     case 2:
+//         r = a - b;
+//         console.log(r);
+//         break;
+
+//     case 3:
+//         r = a * b;
+//         console.log(r);
+//         break;
+
+//     case 4:
+//         r = a / b;
+//         console.log(r);
+//         break;
+
+//     case 5:
+//         r = a % b;
+//         console.log(r);
+//         break;
+
+//     default:
+//         console.log("Invalid choice");
+//         break;
+
+
+// }
+
+let a ="abhishek kumar"
+console.log(a)
+console.log(a.length)
+
+console.log(a.toUpperCase())
---
+++ class62/index.js
@@ -0,0 +1,28 @@
+/* Create a business name generator by combining list of adjectives and shop name and another word
+
+
+Adjectives:
+Crazy 
+Amazing
+Fire 
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+
+
+*/
+let adjectives = ["Crazy", "Amazing", "Fire"]
+let shopNames = ["Engine", "Foods", "Garments"]
+let anotherWords = ["Bros", "Limited", "Hub"]
+
+let businessName = adjectives[Math.floor(Math.random() * adjectives.length)] + shopNames[Math.floor(Math.random() * shopNames.length)] + anotherWords[Math.floor(Math.random() * anotherWords.length)]
+console.log(businessName)
+
