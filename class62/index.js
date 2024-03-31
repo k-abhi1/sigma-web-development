45b285267d9aace554f7cab73cfd91c950a2e4d6
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-03-31 11:12:27 +0530

    65c cmp

--- class62/index.js
+++ class62/index.js
@@ -26,3 +26,24 @@ let anotherWords = ["Bros", "Limited", "Hub"]
 let businessName = adjectives[Math.floor(Math.random() * adjectives.length)] + shopNames[Math.floor(Math.random() * shopNames.length)] + anotherWords[Math.floor(Math.random() * anotherWords.length)]
 console.log(businessName)
 
+/* Create a business name generator by combining list of adjectives and shop name and another word
+
+
+Adjectives:
+Crazy 
+Amazing
+Fire 
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+
+
+*/
