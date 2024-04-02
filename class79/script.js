a2b5ec8a003b42dbed7216a3920baffc4a2ee6a3
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-02 11:21:27 +0530

    80 com

---
+++ class79/script.js
@@ -0,0 +1,41 @@
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <script src="script.js"></script>
+// </body>
+// </html>
+
+
+let a = prompt("Enter first number")
+
+let b = prompt("Enter second number")
+if (isNaN(a) || isNaN(b)) {
+    throw SyntaxError("Sorry this is not allowed")
+}
+
+let sum = parseInt(a) + parseInt(b)
+
+function main(){ 
+    let x = 1;
+    try {
+        console.log("The sum is ", sum * x)
+        return true
+        
+    } catch (error) {
+        console.log("Error aa gaya bhai")
+        return false
+    } 
+    finally{
+        console.log("files are being closed and db connection is being closed")
+    }
+  
+}
+
+let c = main()
