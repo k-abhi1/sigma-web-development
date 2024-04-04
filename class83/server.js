3121104b8c0873a410868587349a7d84f9ff7b8a
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-04 08:52:33 +0530

    node js

---
+++ class83/server.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+
+let a=prompt("Enter the number of students");
+
+let b=prompt("Enter the second of students");
+
+let c=prompt("choce");
+let r;
+
+
+
+switch(c){
+
+    case 1:
+       r=a+b;
+         console.log(r);
+         break;
+
+    
+}
