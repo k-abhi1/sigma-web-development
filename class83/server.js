2d8c81419efc0a4b3fc2aa60323febd2f517b098
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-04 10:27:36 +0530

    node javascript

--- class83/server.js
+++ class83/server.js
@@ -1,21 +1,78 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 
-let a=prompt("Enter the number of students");
+// let a=prompt("Enter the number of students");
 
-let b=prompt("Enter the second of students");
+// let b=prompt("Enter the second of students");
 
-let c=prompt("choce");
-let r;
+// let c=prompt("choce");
+// let r;
 
 
 
-switch(c){
+// switch(c){
 
-    case 1:
-       r=a+b;
-         console.log(r);
-         break;
+//     case 1:
+//        r=a+b;
+//          console.log(r);
+//          break;
+
+//     case 2:
+//         r=a-b;
+//         console.log(r);
+//         break;
+
+//     case 3:
+//         r=a*b;
+//         console.log(r);
+//         break;
+
+//     case 4:
+//         r=a/b;
+//         console.log(r);
+//         break;
+      
+//     default:
+//         console.log("Invalid input");
+//         break;
+
+
 
     
+// }
+
+
+
+console.log("Hello World");
+
+let a = parseFloat(prompt("Enter the first number"));
+let b = parseFloat(prompt("Enter the second number"));
+let c = parseInt(prompt("Choose an operation: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division"));
+
+let r;
+
+switch(c){
+    case 1:
+        r = a + b;
+        console.log("Result: " + r);
+        break;
+    case 2:
+        r = a - b;
+        console.log("Result: " + r);
+        break;
+    case 3:
+        r = a * b;
+        console.log("Result: " + r);
+        break;
+    case 4:
+        if (b !== 0) {
+            r = a / b;
+            console.log("Result: " + r);
+        } else {
+            console.log("Division by zero is not allowed");
+        }
+        break;
+    default:
+        console.log("Invalid input");
+        break;
 }
