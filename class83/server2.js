b17170d5f5adab45bf7dd8e4c4824ac4e0a84666
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-04 12:28:34 +0530

    node js

---
+++ class83/server2.js
@@ -0,0 +1,15 @@
+// var http = require('http');
+
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
+
+console.log("hello world");
+
+
+ let a =4;
+let b =2;
+
+console.log(a+b);
