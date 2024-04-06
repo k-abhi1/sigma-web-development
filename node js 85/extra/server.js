446179955bb354a82d536b3079d87ee1fe36637d
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-06 16:21:30 +0530

    module exports

--- node js 85/extra/server.js
+++ node js 85/extra/server.js
@@ -6,7 +6,7 @@ const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
-});
+})
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
---
+++ node js 86/server.js
@@ -0,0 +1,1 @@
+ // node version manager read 
