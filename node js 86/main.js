6c939459f7ee108769105fee810ef1df83321525
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-08 20:09:48 +0530

    first commit

--- node js 86/main.js
+++ node js 86/main.js
@@ -16,3 +16,27 @@
 // console.log(a, __dirname, __filename)
 
 console.log("This is index.js");
+
+// For example, in an Express app, you can use this to set different error handlers per environment:
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  }
+  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.errorHandler());
+  }
+
+//   Once you have a WebAssembly module, you can use the Node.js WebAssembly object to instantiate it.
+  // Assume add.wasm file exists that contains a single function adding 2 provided arguments
+const fs = require('node:fs');
+const wasmBuffer = fs.readFileSync('/path/to/add.wasm');
+WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+  // Exported function live under instance.exports
+  const { add } = wasmModule.instance.exports;
+  const sum = add(5, 6);
+  console.log(sum); // Outputs: 11
+});
+
+
+  
--- node js 86/my modeule.js
+++ node js 86/my modeule.js
@@ -1,12 +1,21 @@
-export const a = 1 // named export
-export const b = 2 // named export
-export const c = 3 // named export
-export const d = 4 // named export
-export const e = 5 // named export
+// export const a = 1 // named export
+// export const b = 2 // named export
+// export const c = 3 // named export
+// export const d = 4 // named export
+// export const e = 5 // named export
  
-const obj = {
-    x: 5,
-    y: 7
-}
+// const obj = {
+//     x: 5,
+//     y: 7
+// }
 
-export default obj; // default export
+// export default obj; // default export
+
+
+const obj2 ={
+    name:"abhishek kumar singh"
+  
+
+
+}
+console.log(obj2)
