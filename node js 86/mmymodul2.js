a199abe6099c8985e6044378a9ab9dc9b7c32c3a
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-04-14 10:59:42 +0530

    86 node js

---
+++ node js 86/mmymodul2.js
@@ -0,0 +1,21 @@
+// export const a = 1 // named export
+// export const b = 2 // named export
+// export const c = 3 // named export
+// export const d = 4 // named export
+// export const e = 5 // named export
+ 
+// const obj = {
+//     x: 5,
+//     y: 7
+// }
+
+// export default obj; // default export
+
+
+// module.exports = {
+//     a: 1,
+//     b:4
+// }
+
+let c = 56;
+module.exports = c;
