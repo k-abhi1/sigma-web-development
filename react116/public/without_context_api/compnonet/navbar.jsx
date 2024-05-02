c17aa64a7ec2bcdedc279a02527920faf61b058e
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-05-02 23:00:57 +0530

    react120e

---
+++ react116/public/without_context_api/compnonet/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './button'
+
+const Navbar = ({count}) => {
+  return (
+    <>
+    <div>
+      Navbar
+    </div>
+    <Button count={count}/>
+    </>
+  )
+}
+
+export default Navbar
---
+++ react118/src/compnontes/navbar.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { memo } from 'react'
+
+const Navbar = ({adjective, getAdjective}) => {
+    console.log("Navbar is rendered")
+  return (
+    <div>
+      I am a {adjective} Navbar
+      <button onClick={()=>{getAdjective()}}>{getAdjective()}</button>
+    </div>
+  )
+}
+
+export default memo(Navbar)
