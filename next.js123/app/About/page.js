e19bbfae944642e1cc79536c08e9db08db9896df
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-05-03 23:53:36 +0530

    next.js

---
+++ next.js123/app/About/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+    return (
+        <div>
+            <img src="" alt="" />
+          
+            I am about
+        </div>
+    )
+}
+
+export default page
+
+export const metadata = {
+    title: 'About - facebook.com',
+    description: 'facebook is a social media platform',
+}
