2bfa09ca319cb5f44babd547a0dda71652ce4d62
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-05-04 00:04:17 +0530

    next.js

--- next.js125/actions/form.js
+++ next.js125/actions/form.js
@@ -2,6 +2,6 @@
 import fs from "fs/promises"
 export const submitAction = async (e) => {
     console.log(e.get("name"), e.get("add"))
-    let a = await fs.writeFile("harry.txt", `Name is ${e.get("name")} and Address is ${e.get("add")}`) 
+    let a = await fs.writeFile("abhi.txt", `Name is ${e.get("name")} and Address is ${e.get("add")}`) 
 
   }
